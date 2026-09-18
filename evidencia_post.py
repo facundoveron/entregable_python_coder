29b8f04a8f5b8ca0cc93587114210dd199bec889
facundoveron <veronariel98@hotmail.com>
2026-09-18 13:20:13 -0300

    first commit

---
+++ evidencia_post.py
@@ -0,0 +1,10 @@
+# Datos de prueba para mi futuro blog
+titulo = "Mi primer post en Python"
+autor = "Facundo"
+estado = "Entorno configurado correctamente"
+
+print("--- REPORTE DE SISTEMA ---")
+print(f"Post: {titulo}")
+print(f"Autor: {autor}")
+print(f"Estado: {estado}")
+print("--------------------------")
